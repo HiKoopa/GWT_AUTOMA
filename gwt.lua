08fbeface690cf3e8f7d8cff6e245e673f87fb90
koopa <koopa@uangel.com>
2017-11-01 18:06:07 +0900

    move

--- gwt.lua
+++ gwt.lua
@@ -1,54 +1,84 @@
-require "object"
+local OBJ = require "object"
 
 local print_personal_info = function()
 end
 
 local print_map = function()
 end
+--TODO
+local function cal_next_placed(currentManLoc, placeInfo)
+	local next1 = placeInfo[currentManLoc].next1
+	if placeInfo[next1].isPlaced == false then
+		cal_next_placed(next1, placeInfo)
+		return
+	else
+		return next1
+	end
+end
 
-local cal_next_move = function(stepLimit, currentLoc, mapInfo, availableMoveList)
-	if stepLimit < 0 then
+local cal_available_move = function(currentManLoc, placeInfo)
+	local availableMoveList	= { }
+
+	table.insert(availableMoveList, "STOP")
+	print(string.format("1) STOP"))
+
+	table.insert(availableMoveList, cal_next_placed(currentManLoc, placeInfo))
+	print(string.format("2) %s", cal_next_placed(currentManLoc, placeInfo)))
+
+	if placeInfo[currentManLoc].next2 then
+		table.insert(availableMoveList, cal_next_placed(currentManLoc, placeInfo))
+		print(string.format("3) %s", cal_next_placed(currentManLoc, placeInfo)))
+	end
+	
+	return availableMoveList
+end
+
+local function decide_move(stepLimit, currentManLoc, placeInfo)
+	print(string.format("Your current location is %s, Left step : %d", currentManLoc, stepLimit))
+	print()
+	if currentManLoc == "KC" then return end
+
+	if stepLimit <= 0 then return end
+
+	local availableMoveList = cal_available_move(currentManLoc, placeInfo)
+	io.write("Please enter where you want to go : ")
+	nextManLoc = io.read("*number")
+
+	if nextManLoc == 1 then return end
+
+	if availableMoveList[nextManLoc] == nil then
+		print(string.format("The value is not vaild. please re-enter"))
+		decide_move(stepLimit, currentManLoc, placeInfo)
 		return
 	end
 
-	table.insert(availableMoveList, currentLoc)
-	currentLoc.next1 
-	cal_next_move(stepLimit - 1, currentLoc, mapInfo)
+	decide_move(stepLimit - 1, availableMoveList[nextManLoc], placeInfo)
 
-
-	--table.insert(availableMoveList, cal_next_move(stepLimit - 1, currentLoc, mapInfo))
-	--table.insert(availableMoveList, cal_next_move(stepLimit - 1, currentLoc, mapInfo))
-	return 
-end
---[[
-local cal_available_move = function(stepLimit, currentLoc, mapInfo)
-	local availableMoveList = {}
-
-	cal_next_move(stepLimit, currentLoc, mapInfo)
-
-	return availableMoveList
-end
---]]
-local go = function(step)
-	print(step)
+	io.write("\n")
 end
 
-local act_a = function(player)
-	print_personal_info()
-	print_map()
-	--cal_available_move(stepLimit, currentLoc, mapInfo)
-	local availableMoveList = { }
-	cal_next_move(stepLimit, currentLoc, mapInfo, availableMoveList)
-	io.write("Please enter where you want to go : ")
-	go(io.read("*number"))
+local act_a = function(player, placeInfo)
+	player.currentManLoc = "START"
+	placeInfo["A"].isPlaced = true
+	placeInfo["B"].isPlaced = true
+	placeInfo["C"].isPlaced = true
+	placeInfo["D"].isPlaced = true
+	placeInfo["E"].isPlaced = true
+	placeInfo["F"].isPlaced = true
+	placeInfo["G"].isPlaced = true
+	decide_move(3 + player.upgrade.move, player.currentManLoc, placeInfo)
 end
+
 local act_b = function()
 end
+
 local act_c = function()
 end
 
-local start_player_turn = function(player)
-	act_a(player)
+local start_player_turn = function(player, placeInfo)
+	print_personal_info()
+	print_map()
+	act_a(player, placeInfo)
 	act_b(player)
 	act_c(player)
 end
@@ -56,5 +86,9 @@ end
 local start_computer_turn = function()
 end
 
-start_player_turn()
+local init_setting = function(OBJ)
+end
+
+init_setting()
+start_player_turn(OBJ.players.player1, OBJ.place)
 start_computer_turn()
--- object.lua
+++ object.lua
@@ -1,76 +1,58 @@
 local _OBJ = {
---[[
-	MAP_TILE_TYPE = {
-		NOTHING = "NOTHING", 
-		NETURAL_BUILDING = "NETURAL_BUILDING", 
-		PRIVATE_BUILDING = "PRIVATE_BUILDING", 
-		HAZARD = "HAZARD", 
-		TEEPEE = "TEEPEE",
-	}
---]]
-}
-
-return _OBJ
-
---[[
-object
-	map = {
-		place = {
-				--initial
-["START"]	= { next1 = "A", },
-["A"]		= { next1 = "A1", next2 = "HZF1", },
-["A1"] 		= { next1 = "A2", },
-["A2"] 		= { next1 = "A3", },
-["A3"] 		= { next1 = "B", },
-["HZF1"]	= { next1 = "HZF2", },
-["HZF2"] 	= { next1 = "HZF3", },
-["HZF3"] 	= { next1 = "HZF4", },
-["HZF4"] 	= { next1 = "HZFA1", },
-["HZFA1"] 	= { next1 = "HZFA2", addAct = TODO, },
-["HZFA2"] 	= { next1 = "B", addAct = TODO, },
-["B"] 		= { next1 = "B1", next2 = "HZD1" },
-["B1"] 		= { next1 = "B2", bush = true, },
-["B2"] 		= { next1 = "B3", },
-["B3"] 		= { next1 = "C", },
-["HZD1"] 	= { next1 = "HZD2", },
-["HZD2"] 	= { next1 = "HZD3", },
-["HZD3"] 	= { next1 = "HZD4", },
-["HZD4"] 	= { next1 = "HZDB1", },
-["HZDB1"] 	= { next1 = "C", addAct = TODO, },
-["C"] 		= { next1 = "C1", next2 = "T", },
-["C1"] 		= { next1 = "C2", bush = true, },
-["C2"] 		= { next1 = "E", bush = true, },
-["T"] 		= { next1 = "D", next2 = "TP1", },
-["D"] 		= { next1 = "E", },
-["TP1"] 	= { next1 = "TP2", },
-["TP2"] 	= { next1 = "TP3", },
-["TP3"] 	= { next1 = "TP4", },
-["TP4"] 	= { next1 = "TP5", },
-["TP5"] 	= { next1 = "TP6", },
-["TP6"] 	= { next1 = "TPT1", },
-["TPT1"] 	= { next1 = "TPT2", addAct = true, },
-["TPT2"] 	= { next1 = "E", addAct = true, },
-["E"] 		= { next1 = "E1", next2 = "HZR1", },
-["E1"] 		= { next1 = "E2", bush = true, },
-["E2"] 		= { next1 = "F", bush = true, },
-["HZR1"] 	= { next1 = "HZR2", },
-["HZR2"] 	= { next1 = "HZR3", },
-["HZR3"] 	= { next1 = "HZR4", },
-["HZR4"] 	= { next1 = "HZRE1", },
-["HZRE1"] 	= { next1 = "HZRE2", addAct = TODO, },
-["HZRE2"] 	= { next1 = "F", addAct = TODO, },
-["F"] 		= { next1 = "F1", next2 = "F2", },
-["F1"] 		= { next1 = "G", bush = true, },
-["F2"] 		= { next1 = "G", },
-["G"] 		= { next1 = "G1", next2 = "G2", },
-["G1"] 		= { next1 = "KC", },
-["G2"] 		= { next1 = "KC", },
-			}
-		}
-	}, 
+	place = {
+		["START"]	= { next1 = "A", },
+		["A"]		= { isPlaced = false, next1 = "A1", next2 = "HZF1", },
+		["A1"] 		= { isPlaced = false, next1 = "A2", },
+		["A2"] 		= { isPlaced = false, next1 = "A3", },
+		["A3"] 		= { isPlaced = false, next1 = "B", },
+		["HZF1"]	= { isPlaced = false, next1 = "HZF2", },
+		["HZF2"] 	= { isPlaced = false, next1 = "HZF3", },
+		["HZF3"] 	= { isPlaced = false, next1 = "HZF4", },
+		["HZF4"] 	= { isPlaced = false, next1 = "HZFA1", },
+		["HZFA1"] 	= { isPlaced = false, next1 = "HZFA2", addAct = TODO, },
+		["HZFA2"] 	= { isPlaced = false, next1 = "B", addAct = TODO, },
+		["B"] 		= { isPlaced = false, next1 = "B1", next2 = "HZD1" },
+		["B1"] 		= { isPlaced = false, next1 = "B2", bush = true, },
+		["B2"] 		= { isPlaced = false, next1 = "B3", },
+		["B3"] 		= { isPlaced = false, next1 = "C", },
+		["HZD1"] 	= { isPlaced = false, next1 = "HZD2", },
+		["HZD2"] 	= { isPlaced = false, next1 = "HZD3", },
+		["HZD3"] 	= { isPlaced = false, next1 = "HZD4", },
+		["HZD4"] 	= { isPlaced = false, next1 = "HZDB1", },
+		["HZDB1"] 	= { isPlaced = false, next1 = "C", addAct = TODO, },
+		["C"] 		= { isPlaced = false, next1 = "C1", next2 = "T", },
+		["C1"] 		= { isPlaced = false, next1 = "C2", bush = true, },
+		["C2"] 		= { isPlaced = false, next1 = "E", bush = true, },
+		["T"] 		= { isPlaced = false, next1 = "D", next2 = "TP1", },
+		["D"] 		= { isPlaced = false, next1 = "E", },
+		["TP1"] 	= { isPlaced = false, next1 = "TP2", },
+		["TP2"] 	= { isPlaced = false, next1 = "TP3", },
+		["TP3"] 	= { isPlaced = false, next1 = "TP4", },
+		["TP4"] 	= { isPlaced = false, next1 = "TP5", },
+		["TP5"] 	= { isPlaced = false, next1 = "TP6", },
+		["TP6"] 	= { isPlaced = false, next1 = "TPT1", },
+		["TPT1"] 	= { isPlaced = false, next1 = "TPT2", addAct = true, },
+		["TPT2"] 	= { isPlaced = false, next1 = "E", addAct = true, },
+		["E"] 		= { isPlaced = false, next1 = "E1", next2 = "HZR1", },
+		["E1"] 		= { isPlaced = false, next1 = "E2", bush = true, },
+		["E2"] 		= { isPlaced = false, next1 = "F", bush = true, },
+		["HZR1"] 	= { isPlaced = false, next1 = "HZR2", },
+		["HZR2"] 	= { isPlaced = false, next1 = "HZR3", },
+		["HZR3"] 	= { isPlaced = false, next1 = "HZR4", },
+		["HZR4"] 	= { isPlaced = false, next1 = "HZRE1", },
+		["HZRE1"] 	= { isPlaced = false, next1 = "HZRE2", addAct = TODO, },
+		["HZRE2"] 	= { isPlaced = false, next1 = "F", addAct = TODO, },
+		["F"] 		= { isPlaced = false, next1 = "F1", next2 = "F2", },
+		["F1"] 		= { isPlaced = false, next1 = "G", bush = true, },
+		["F2"] 		= { isPlaced = false, next1 = "G", },
+		["G"] 		= { isPlaced = false, next1 = "G1", next2 = "G2", },
+		["G1"] 		= { isPlaced = false, next1 = "KC", },
+		["G2"] 		= { isPlaced = false, next1 = "KC", },
+		["KC"]		= { },
+	},
 	kc = {
 	},
-	tiles = {
+	tileStack = {
 	},
 	city = {
 	}, 
@@ -84,6 +66,8 @@ object
 	}, 
 	players = {
 		player1 = {
+			currentManLoc,
+			currentEngLoc,
 			money, 
 			availableBuilding, 
 			drawDeck, 
@@ -91,8 +75,24 @@ object
 			discardPile, 
 			removedCard, 
 			objectiveCard,
+			hztp,
+			upgrade = { dollar = 1, d_d = 1, cert = 0, engine = 0, remove = 0, move = 0, hand = 0, cert = 0, },
+			employee = { cw = 1, cf = 1, eg = 1 },
+			cert = 0,
 		},
 		computer = {
 		},
 	},
+}
+
+return _OBJ
+
+--[[
+	MAP_TILE_TYPE = {
+		NOTHING = "NOTHING", 
+		NETURAL_BUILDING = "NETURAL_BUILDING", 
+		PRIVATE_BUILDING = "PRIVATE_BUILDING", 
+		HAZARD = "HAZARD", 
+		TEEPEE = "TEEPEE",
+	}
 --]]
